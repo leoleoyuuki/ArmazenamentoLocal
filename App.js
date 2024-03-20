7d60deb31bd437f6202b738e236bafaf2cb44c83
leoleoyuuki <rm550373@fiap.com.br>
2024-03-20 10:47:31 -0300

    .

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {AsyncStorage} from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Cadastro</Text>
+      <TextInput placeholder="Digite o nome do produto" style={styles.input} />
+      <TextInput placeholder="Digite o preço do produto" style={styles.input}/>
+
+      <TouchableOpacity style={styles.btn}>
+        <Text style={{color: '#fff', fontSize: 20}}>CADASTRAR</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +18,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10
+  },
+  input: {
+    borderWidth: 1,
+    width: 300,
+    height: 30,
+    borderRadius: 5,
+    paddingLeft: 5
+  },
+  btn: {
+    borderWidth: 1,
+    width: 300,
+    height: 30,
+    borderRadius: 50,
+    backgroundColor: '#000',
+    textAlign:'center'
   },
 });
